d62da2afaf7dd24ac7f87abce057e2350b8b4f51
seturi <48719589+seturi@users.noreply.github.com>
2023-05-14 20:15:39 +0900

    Chapter06 Practice

---
+++ Chapter06/Lab6/ChildView.cpp
@@ -0,0 +1,160 @@
+﻿// B911037 김순욱
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Lab6.h"
+#include "ChildView.h"
+#include "MainFrm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_color = RGB(255, 0, 0);
+	m_text = true;
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_COLOR_RED, &CChildView::OnColorRed)
+	ON_COMMAND(ID_COLOR_GREEN, &CChildView::OnColorGreen)
+	ON_COMMAND(ID_COLOR_BLUE, &CChildView::OnColorBlue)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_RED, &CChildView::OnUpdateColorRed)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_GREEN, &CChildView::OnUpdateColorGreen)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_BLUE, &CChildView::OnUpdateColorBlue)
+	ON_COMMAND(ID_COLOR_TEXTON, &CChildView::OnColorTexton)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_TEXTON, &CChildView::OnUpdateColorTexton)
+	ON_WM_CONTEXTMENU()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this);
+
+	CFont font;
+	font.CreatePointFont(300, _T("궁서"));
+	dc.SelectObject(&font);
+	dc.SetTextColor(m_color);
+
+	CRect rect;
+	GetClientRect(&rect);
+	CString str = _T("메뉴 테스트");
+	dc.DrawText(str, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+}
+
+
+
+void CChildView::OnColorRed()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(255, 0, 0);
+	Invalidate();
+}
+
+
+void CChildView::OnColorGreen()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(0, 255, 0);
+	Invalidate();
+}
+
+
+void CChildView::OnColorBlue()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(0, 0, 255);
+	Invalidate();
+}
+
+
+void CChildView::OnUpdateColorRed(CCmdUI* pCmdUI)
+{
+	// TODO: 여기에 명령 업데이트 UI 처리기 코드를 추가합니다.
+	pCmdUI->SetCheck(m_color == RGB(255, 0, 0));
+}
+
+
+void CChildView::OnUpdateColorGreen(CCmdUI* pCmdUI)
+{
+	// TODO: 여기에 명령 업데이트 UI 처리기 코드를 추가합니다.
+	pCmdUI->SetCheck(m_color == RGB(0, 255, 0));
+}
+
+
+void CChildView::OnUpdateColorBlue(CCmdUI* pCmdUI)
+{
+	// TODO: 여기에 명령 업데이트 UI 처리기 코드를 추가합니다.
+	pCmdUI->SetCheck(m_color == RGB(0, 0, 255));
+}
+
+
+void CChildView::OnColorTexton()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_text = !m_text;
+}
+
+
+void CChildView::OnUpdateColorTexton(CCmdUI* pCmdUI)
+{
+	// TODO: 여기에 명령 업데이트 UI 처리기 코드를 추가합니다.
+	pCmdUI->SetRadio(m_text == true);
+	if (m_text)
+		pCmdUI->SetText(CString("Text On!"));
+	else
+		pCmdUI->SetText(CString("Text Off!"));
+}
+
+
+void CChildView::OnContextMenu(CWnd* pWnd, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	CMenu menu;
+	menu.LoadMenu(IDR_MAINFRAME);
+	CMenu* pMenu = menu.GetSubMenu(4);
+	pMenu->TrackPopupMenu(
+		TPM_LEFTALIGN | TPM_RIGHTBUTTON,
+		point.x, point.y, AfxGetMainWnd());
+}
+
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	CString str;
+	str.Format(_T("(%4d, %4d)"), point.x, point.y);
+
+	CMainFrame* pMainFrame = (CMainFrame*)AfxGetMainWnd();
+	pMainFrame->m_wndStatusBar.SetPaneText(1, str);
+}
---
+++ Chapter06/Lab6/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Lab6.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define ID_INDICATOR_POS                129
+#define IDR_Lab6TYPE                    130
+#define ID_COLOR_RED                    32771
+#define ID_COLOR_GREEN                  32772
+#define ID_COLOR_BLUE                   32773
+#define ID_COLOR_TEXTON                 32774
+#define ID_BUTTON32779                  32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
